a02c4e555d79d21f3962c364b5dd667cf40e8144
Matt Basile <matthewbasile2@gmail.com>
2019-03-11 08:39:33 -0400

    Looking up org returns donor objects as well.

--- models/organization-model.js
+++ models/organization-model.js
@@ -5,7 +5,8 @@ const db = knex(knexConfig.development);
 
 module.exports={
     find,
-    findOrgById
+    findOrgById,
+    findDonor
 }
 
 function find(){
@@ -13,7 +14,8 @@ function find(){
 }
 async function findOrgById(id){
     const org = await db('organizations').where({id}).first().select('id', 'username', 'org_name', 'email')
-    const donors = await db('org_donors').where('org_id', id)
+    const donors = await db('org_donors').where('org_id', id).map(donor => findDonor(donor.donor_id))
+    // const newDonors = donors
     const campaigns = await ( db('campaigns').where( 'org_id', id ))
     return ({
         org,
@@ -22,7 +24,10 @@ async function findOrgById(id){
      })
 }
 
-
+async function findDonor(id){
+    const donor = await db('donors').where('id', id)
+    return(donor)
+}
 
 // function findOrgDonors(id){
 //     return db('org_donors')
--- routes/organizations-routes.js
+++ routes/organizations-routes.js
@@ -4,11 +4,13 @@ const Orgs = require('../models/organization-model.js');
 module.exports= server =>{
     server.get('/api/organizations', getOrganizations)
     server.get('/api/organizations/:id', getAOrg)
+    server.get('/api/donor/:id', getADonor)
 }
 
 const getTest = (req, res) =>{
     res.status(200).json('checking in');
 }
+
 
 const getOrganizations = (req, res) =>{
     Orgs.find()
@@ -22,6 +24,15 @@ const getOrganizations = (req, res) =>{
 
 const getAOrg = async(req, res)=>{
     Orgs.findOrgById((req.params.id))
+    .then(data =>{
+        res.status(200).json(data) 
+    })
+    .catch(err =>{
+        res.status(500).json({ message: ` Failed to get that Org`, error: err });
+    })
+}
+const getADonor = async(req, res)=>{
+    Orgs.findDonor((req.params.id))
     .then(data =>{
         res.status(200).json(data) 
     })
